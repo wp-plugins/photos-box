189a8831bb43e56e6ebf64afdfdbe8be95596b91
demoswebsites <demoswebsites@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-05 04:10:23 +0000

    update photos-box.php
    git-svn-id: https://plugins.svn.wordpress.org/photos-box/trunk@1038622 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- photos-box.php
+++ photos-box.php
@@ -4,8 +4,8 @@ Plugin Name: Photos Box
 Description: Photos Box is an advanced plugin with a list of options for gallery. View Popup, Slideshow Popup
 Plugin URI: https://wordpress.org/plugins/photos-box
 Author: Hoa Lu
-Author URI:
-Version: 1.0.3
+Author URI: http://photosbox.tk/
+Version: 1.0.4
 License: GPL-2.0+
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
