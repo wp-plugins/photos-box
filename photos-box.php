4345c50317f715202a3bcdd5bd63f5e1a1cb2c05
demoswebsites <demoswebsites@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-21 03:53:38 +0000

    update core
    git-svn-id: https://plugins.svn.wordpress.org/photos-box/trunk@1140113 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- photos-box.php
+++ photos-box.php
@@ -17,18 +17,23 @@ define('WP_PB_PATH_INCLUDES', dirname(__FILE__).'/includes' );
 define('WP_PB_URL', plugins_url('', __FILE__).'/' ); 
 define('WP_PB_URL_IMAGES', WP_PB_URL.'images/' ); 
 
-add_filter("plugin_action_links_".plugin_basename(__FILE__), "photos_box_plugin_actions", 10, 4);
-function photos_box_plugin_actions( $actions, $plugin_file, $plugin_data, $context ) {
-	array_unshift($actions, "<a href=\"http://photosbox.tk/documents\" target=\"_blank\">".__("Documents")."</a>");
-	array_unshift($actions, "<a href=\"options-general.php?page=photos-box-setting\">".__("Settings")."</a>");
-	return $actions;
-}
-
 if( is_admin() ){
 	require WP_PB_PATH_INCLUDES.'/config.php';
 	
 	$action = isset($_GET['action'])?$_GET['action']:'';
 	$page 	= isset($_GET['page'])?$_GET['page']:'';
+	$plugins = preg_match('/plugins.php/i',$_SERVER['REQUEST_URI']);
+	
+	if( $plugins ){
+		
+		add_filter("plugin_action_links_".plugin_basename(__FILE__), "photos_box_plugin_actions", 10, 4);
+		function photos_box_plugin_actions( $actions, $plugin_file, $plugin_data, $context ) {
+			array_unshift($actions, "<a href=\"http://photosbox.tk/donate\" target=\"_blank\">".__("Donate")."</a>");
+			array_unshift($actions, "<a href=\"http://photosbox.tk/documents\" target=\"_blank\">".__("Documents")."</a>");
+			array_unshift($actions, "<a href=\"options-general.php?page=photos-box-setting\">".__("Settings")."</a>");
+			return $actions;
+		}
+	}
 	
 	if( $action == 'edit' ){
 		require WP_PB_PATH_INCLUDES.'/admin.php';
