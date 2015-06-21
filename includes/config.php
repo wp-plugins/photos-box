7a8a616b1dc423e68f9d7847da36d778f382dcb0
demoswebsites <demoswebsites@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-21 01:45:20 +0000

    update core
    git-svn-id: https://plugins.svn.wordpress.org/photos-box/trunk@1184898 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/config.php
+++ includes/config.php
@@ -14,6 +14,8 @@ if( !function_exists('photo_box_add_options_page') ){
 		);
 		
 		// when has error 
+		$errors = array();
+		$messages = array();
 	}
 }
 add_action('admin_menu','photo_box_add_options_page');
