4f6665ae3f873b6fcf41bd4cbd62f2f5c777b135
demoswebsites <demoswebsites@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-30 01:53:59 +0000

    update core
    git-svn-id: https://plugins.svn.wordpress.org/photos-box/trunk@1209381 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/config.php
+++ includes/config.php
@@ -9,11 +9,11 @@ defined('ABSPATH') or die('<meta http-equiv="refresh" content="0;url='.WP_PB_URL
 if( !function_exists('photo_box_add_options_page') ){
 	function photo_box_add_options_page() {
 		add_options_page(
-			'Photo Box Settings',			// The text to be displayed in the browser title bar
-			'Photo Box',					// The text to be used for the menu
-			'manage_options',				// The required capability of users to access this menu
-			'photo-box-setting',			// The slug by which this menu item is accessible
-			'photo_box_setting_display'	// The name of the function used to display the page content
+			'Photo Box Settings',
+			'Photo Box',
+			'manage_options',
+			'photo-box-setting',
+			'photo_box_setting_display'
 		);
 		$errors = array();
 		$json = array();
