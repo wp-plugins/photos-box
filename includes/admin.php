b0c3e1bcfbd5a6342ce9d8760f4b0dc02d42b36c
demoswebsites <demoswebsites@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-08 14:25:12 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/photos-box/trunk@1215838 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin.php
+++ includes/admin.php
@@ -21,7 +21,7 @@ function photo_box_gallery_setting(){
 		</label>
 		<label class="setting">
 			<span><?php _e('Slideshow Speed'); ?></span>
-			<input data-setting="slideshow_speed" type="text" value="2500" style="width:100px;"/>
+			<input data-setting="slideshow_speed" type="text" value="2500" style="width:100px; "/>
 		</label>
 	</script>
 	<script>
