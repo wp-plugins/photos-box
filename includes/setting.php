c106b0c38b213be4bb071dbbaf7338c8235a57e1
demoswebsites <demoswebsites@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-09 14:57:18 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/photos-box/trunk@1216394 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/setting.php
+++ includes/setting.php
@@ -17,7 +17,7 @@ function photo_box_init_theme_opotion() {
 	add_settings_field('photo_box_display[autopopup_times]', 'Auto Popup Times','photo_box_display_autopopup_times','photo_box-display-section','photo_box_display_section');
 	//add_settings_field('photo_box_display[autohide]', 'Auto Hide','photo_box_display_autohide','photo_box-display-section','photo_box_display_section');
 	//add_settings_field('photo_box_display[custom_lang]', 'Custom Lang','photo_box_display_autohide','photo_box-display-section','photo_box_display_section');
-	//add_settings_field('photo_box_display[custom_lang_title]', 'Custom Lang','photo_box_display_autohide','photo_box-display-section','photo_box_display_section');
+	//add_settings_field('photo_box_display[custom_lang_title]', 'Custom Lang Title','photo_box_display_autohide','photo_box-display-section','photo_box_display_section');
 	
 	register_setting( 'photo_box_settings','photo_box_display');
 	
