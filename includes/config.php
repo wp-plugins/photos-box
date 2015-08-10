a82088267dc541cddf546313d53f73843c8f8b46
demoswebsites <demoswebsites@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 15:18:24 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/photos-box/trunk@1217356 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/config.php
+++ includes/config.php
@@ -16,7 +16,7 @@ if( !function_exists('photo_box_add_options_page') ){
 			'photo_box_setting_display'
 		);
 		$errors = array('type' => 0, 'color' => '' );
-		$json = array('data'=>'');
+		$json = array('data'=>'', 't' => '');
 		$messages = array( 'success'=> true, 'message' => '' );
 	}
 }
