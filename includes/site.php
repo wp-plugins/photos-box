bf4a5a5dfb2cfe19f1f42fd7cdadf9f5e7d6203c
demoswebsites <demoswebsites@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-12 02:00:07 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/photos-box/trunk@1218734 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/site.php
+++ includes/site.php
@@ -29,7 +29,7 @@ function photo_box_shortcode($val, $attr){
 		'slideshow_speed' => 2500,
 		'slide_style' => 'none',
 		'slide_style_pre' => '',
-		'slide_style_e' => '',
+		'slide_style_eae' => '',
 	), $attr));
 	
 	if( $type != 'photosbox' ) return '';
